c7eef85a881f5a394e0dc21210cf58d16ff6237c
zhaowenkai <799480165@qq.com>
2024-05-09 10:25:56 +0800

    add helm chart

--- src/Chaldea.Fate.RhoAias.IngressController/RhoAiasIngressControllerOptions.cs
+++ src/Chaldea.Fate.RhoAias.IngressController/RhoAiasIngressControllerOptions.cs
@@ -3,9 +3,9 @@
 public class RhoAiasIngressControllerOptions
 {
 	public bool Enable { get; set; } = false;
-	public string ControllerClass { get; set; } = "microsoft.com/ingress-yarp";
+	public string ControllerClass { get; set; } = "chaldea.cn/ingress-rho-aias";
 	public bool ServerCertificates { get; set; } = false;
-	public string DefaultSslCertificate { get; set; } = "yarp/yarp-ingress-tls";
-	public string ControllerServiceName { get; set; } = "ingress-yarp-controller";
-	public string ControllerServiceNamespace { get; set; } = "yarp";
+	public string DefaultSslCertificate { get; set; } = "rho-aias/rho-aias-ingress-tls";
+	public string ControllerServiceName { get; set; } = "ingress-rho-aias-controller";
+	public string ControllerServiceNamespace { get; set; } = "rho-aias";
 }
